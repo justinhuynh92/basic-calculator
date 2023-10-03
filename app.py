675b8c94beda0c84776f91f4fbf17776aa25f1dc
Justin Huynh <justinhuynh639@gmail.com>
2023-10-03 13:25:43 -0700

    concat operators

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ def add(a, b):
     answer = a + b
     print(str(a) + " + " + str(b) + " = " + str(answer))
 
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer))
+
+def div(a, b):
+    answer = a / b
+    print(str(a) + " / " + str(b) + " = " + str(answer))
 #print options to the user
 #ask for values
 #call the functions

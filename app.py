e8a8ed2f25b8a8b3e5ec6de4f1fa189240686a49
Justin Huynh <justinhuynh639@gmail.com>
2023-10-03 13:27:43 -0700

    ask user for input

--- app.py
+++ app.py
@@ -14,7 +14,16 @@ def mul(a, b):
 def div(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))
+
 #print options to the user
+print("A. Addition")
+print("B. Subtraction")
+print("C. Multiplication")
+print("D. Division")
+print("E. Exit")
+
 #ask for values
+choice = input("input your choice")
+
 #call the functions
 #while loop to continue the program until the user wants to exit
